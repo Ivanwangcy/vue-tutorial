ae93c71758366899579fbf81ce04b2333e74353b
Ivan <ivan.wangcy@gmail.com>
2016-09-21 15:51:54 +0800

    vue webpack config

--- tutorial05/build/webpack.base.config.js
+++ tutorial05/build/webpack.base.config.js
@@ -7,7 +7,14 @@ module.exports = {
   devtool: '#source-map',
   entry: {
     app: './src/client-entry.js',
-    vendor: ['vue', 'vue-router', 'vuex', 'firebase', 'lru-cache', 'es6-promise']
+    vendor: [
+      'vue', // vue 2.0 核心代码
+      'vue-router', // vue 路由
+      'vuex', // 单项数据流 flux 框架
+      'firebase', //
+      'lru-cache', //
+      'es6-promise' // ES6 promise 兼容库
+    ]
   },
   output: {
     path: path.resolve(__dirname, '../dist'),
