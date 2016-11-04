7826a229bf4975728e10556ee762be215111e150
Ivan <wcy5211678@163.com>
2016-11-04 10:47:59 +0800

    remove code

--- tutorial07/src/main.js
+++ tutorial07/src/main.js
@@ -3,20 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'font-awesome-loader'
-import VueResource from 'vue-resource'
 
-Vue.use(VueResource)
-
-
-// store.commit('loading', true)
-// Vue.http.interceptors.push((request, next) => {
-//   // 请求前的处理逻辑
-//
-//   next((response) => {
-//
-//     return response
-//   })
-// })
 
 new Vue({
   el: '#app',
